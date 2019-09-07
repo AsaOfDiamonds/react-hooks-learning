0c559545fddf40cb4a99816bcf56fe6d84285f77
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-09-06 19:01:05 -0600

    multiple state variables

---
+++ src/components/MultipleState.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function ExampleWithManyStates() {
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks'}]);
+}
