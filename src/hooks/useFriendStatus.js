82b2df08481952aba0ea203f2f56527f70506116
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-09-08 11:51:25 -0600

    useFriendStatusHook started

---
+++ src/hooks/useFriendStatus.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function useFriendStatus(friendID) {
+  const [isOnline, setIsOnline] = useState(null);
+
+  function handleStatusChange(status) {
+    setIsOnline(status.isOnline);
+  }
+
+  
+}
